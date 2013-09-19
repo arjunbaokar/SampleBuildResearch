bfe1bdab4029041a4ec9206efd474063e6e449bf
Arjun Baokar <arjunbaokar@gmail.com>
2013-09-18 18:55:35 -0700

    added to Matrix and Vector

--- Matrix.py
+++ Matrix.py
@@ -1,5 +1,28 @@
+'''
+Optimized for getting columns, not rows.
+'''
 class Matrix():
 
+	def __init__(self, vectors):
+		self.vectorList = vectors
 
-	def __init__(self, list_of_rows):
-		self.listOfRows = list_of_rows
+	def numCols():
+		return len(self.vectorList[0])
+
+	def numRows():
+		return len(self.vectorList)
+
+	def rank():
+		return None # IMPLEMENT THIS
+
+	def getRow(n):
+		buildVector = Vector()
+		for i in range len(self.vectorList):
+			buildVector.add(self.vectorList[i][n])
+		return self.vectorList[i]
+
+	def getCol(j):
+		return self.vectorList[j]
+
+	def get(i,j):
+		return self.vectorList[i].get[j]
--- Vector.py
+++ Vector.py
@@ -1,4 +1,16 @@
 class Vector():
 
-	def __init__(self, row_values):
-		self.rowValues = row_values
+	def __init__(self, row_values = None):
+		if rowValues != None:
+			self.rowValues = row_values
+		else
+			self.rowValues = []
+
+	def __len__(self):
+		return len(self.rowValues)
+
+	def get(n):
+		return rowValues[n]
+
+	def add(n):
+		rowValues += [n]
