c68f763120bf39c4703f6e7af8df4ceed577c653
Swetha <toswethams@gmail.com>
2013-09-18 18:40:08 -0700

    Added Matrix.py and Vector.py

---
+++ Matrix.py
@@ -0,0 +1,5 @@
+class Matrix():
+
+
+	def __init__(self, list_of_rows):
+		self.listOfRows = list_of_rows
---
+++ Vector.py
@@ -0,0 +1,4 @@
+class Vector():
+
+	def __init__(self, row_values):
+		self.rowValues = row_values
