f733ec3af5a0858d959995750c76511c0b19372d
Arjun Baokar <arjunbaokar@gmail.com>
2013-09-27 13:15:23 -0700

    wrote some more on Bourgain

--- BourgainEmbeddingAlgorithm.py
+++ BourgainEmbeddingAlgorithm.py
@@ -1,4 +1,4 @@
-from random import randrange
+import random
 import math
 
 class BourgainEmbeddingAlgorithm(Algorithm):
@@ -10,15 +10,18 @@ class BourgainEmbeddingAlgorithm(Algorithm):
 	def runAlgorithm():
 		m1 = matrix.numCols()
 		m2 = math.log(m1)
+		randSet = set()
 		for i in range(1,m1+1):
 			for j in range(1,m2+1):
-				print 'UNDERSTAND AND DO THIS' #TODO finish this
+				 if (useCurrentNumber(i)):
+				 	randSet.add(1239481293047129834701293) #x in V, find a way to figure out what x in V is
+
 
 
 
 	# Decides if current number will be used in algorithm. Returns true with P(X) = 2**-i, where i is supplied by the caller
 	def useCurrentNumber(i):
-		if 0 == randrange(2**i):
+		if 1 == random.randrange(2**i):
 			return True
 		return False
 
