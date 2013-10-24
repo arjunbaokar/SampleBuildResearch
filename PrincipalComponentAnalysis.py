3e196fdb3e5db754a9f64fc4970a2f7f9a7b1c00
Arjun Baokar <arjunbaokar@gmail.com>
2013-10-24 02:36:54 -0700

    PCA fully working with nodes, trained on 100 random variables

--- PrincipalComponentAnalysis.py
+++ PrincipalComponentAnalysis.py
@@ -5,15 +5,23 @@ import api
 class PrincipalComponentAnalysis(api.Algorithm):
 
 	def __init__(self, inputDim, targetMatrix, desiredDim):
-		self.targetDimension = desiredDim
-		self.matrix = targetMatrix
 		self.startingDimension = inputDim
+		self.matrix = targetMatrix
+		self.targetDimension = desiredDim
 
 	def setTargetMatrix(targetMatrix):
 		self.matrix = targetMatrix
 
 	def setTargetDimension(desiredDim):
 		self.targetDimension = desiredDim
+
+	def trainNode(self, node):
+		if node.is_trainable():
+			x = numpy.random.random((100, self.startingDimension))  # 25 variables, 100 observations
+			node.train(x)
+			node.stop_training()
+			print "output_dim: " + str(node.output_dim)
+			print "var: " + str(node.explained_variance)
 
 	def runAlgorithm(self):
 		if (self.targetDimension == None):
@@ -27,5 +35,6 @@ class PrincipalComponentAnalysis(api.Algorithm):
 			
 			prePcaMatrix = numpy.array(twoDimList, dtype='float64')
 			pcaNode = mdp.nodes.PCANode(input_dim=self.startingDimension ,output_dim=self.targetDimension, dtype='float64')
-			pcaResult = mdp.pca(prePcaMatrix)
+			self.trainNode(pcaNode)
+			pcaResult = pcaNode(prePcaMatrix)
 			return pcaResult
